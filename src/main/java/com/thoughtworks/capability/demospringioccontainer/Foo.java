06f3518dd654d09c4277f6c31f47e055ec2a54ff
D Yang <moxuan812@foxmail.com>
2021-03-01 17:25:36 +0800

    [Yang Di] feat: third solution with implement ApplicationContextAware and InitializingBean

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,14 +1,30 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Foo {
+public class Foo implements ApplicationContextAware, InitializingBean {
 
     private Bar bar;
 
-    public Foo(Bar bar) {
-        this.bar = bar;
+    private ApplicationContext context;
+
+    public Bar getBar() {
+        return bar;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        bar = context.getBean(Bar.class);
+    }
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) throws BeansException {
+        this.context = context;
     }
 
     public void hi() {
